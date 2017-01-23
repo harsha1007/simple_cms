27561a2af878cc35dc59203136cdfc6cd3c57299
= <skunapa@ncsu.edu>
2017-01-23 15:58:45 -0500

    Database migrations for user, subject, page and section

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,4 +4,13 @@ class DemoController < ApplicationController
 
   def index
   end
+
+  def hello
+  	@array = [1,2,3,4,5]
+
+  end
+
+  def other_hello
+  	redirect_to(controller: 'demo', action: 'index')
+  end
 end
